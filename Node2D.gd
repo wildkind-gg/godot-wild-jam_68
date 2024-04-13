9e48dea40a33c872808eac89795c94b7749c2eb4
Zachary Snader <z@wildkind.gg>
2024-04-12 21:50:35 -0600

    Added a comment test
    Added a comment just below the initial "hello world" comment test.

--- Node2D.gd
+++ Node2D.gd
@@ -3,4 +3,6 @@ extends Node2D
 func _ready():
 	$Label.text = "hello, world"
 	
-	# This comment is a test 
+	# This comment is a test - C
+	
+	# Testing a comment back // let's call the game "lofi beats to slay monsters to" even if we don't end up with lofi beats. in my brain this is funny. - Z
