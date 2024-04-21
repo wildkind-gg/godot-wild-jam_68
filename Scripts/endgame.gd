14b7a702283a7689cfc0a43f13252f2957403d2a
CHaagFernandez <cfern721@gmail.com>
2024-04-21 12:41:58 -0600

    testing_broken_shit

--- Scripts/endgame.gd
+++ Scripts/endgame.gd
@@ -9,7 +9,7 @@ func _on_animation_player_animation_finished(anim_name):
 		await get_tree().create_timer(2).timeout
 		$AnimationPlayer.play("fade in button")
 	if anim_name == "fade out":
-		get_tree().change_scene_to_file("res://Scenes/bounty_board/bounty_board.tscn")
+		get_tree().change_scene_to_file("res://Scenes/menus/main_menu.tscn")
 		
 
 func _on_button_pressed():
